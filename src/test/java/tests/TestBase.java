7a052ea1cf4909235b28e2340f65ee04d835802f
IgorGerasimenko <58310275+IgorGerasimenko@users.noreply.github.com>
2021-06-21 23:21:22 +0400

    Добавил запись видео

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -7,25 +7,43 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class TestBase {
 
     @BeforeAll
     static void setup() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         Configuration.startMaximized = true;
+
+        //это настройки Selenoid для записи видео
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
     @AfterEach
-    public void tearDown(){
+    public void tearDown() {
+        String sessionId = getSessionId();
+
         Attach.screenshotAs("LastSCR");
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Selenide.closeWebDriver();
+
+        Attach.addVideo(sessionId);
     }
 
-
+    public static String getSessionId() {
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+    }
 
 
 }
